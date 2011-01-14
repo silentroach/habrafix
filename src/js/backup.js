17a00250a68be72a4378697ba196e8a4daaecc42
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-01-14 23:52:13 +0300

    backup for habrahabr textareas

---
+++ src/js/backup.js
@@ -0,0 +1,48 @@
+( function() {
+
+	var 
+		textareas = document.querySelectorAll('textarea[name]'),
+		tforms = [];
+	
+	var storagePath = function(element) {
+		return location.pathname + '#' + element.name;
+	}
+	
+	for (var i = 0; i < textareas.length; i++) {
+		var element = textareas[i];
+		
+		if (!element.form) {
+			continue;
+		}
+		
+		var value = localStorage.getItem(storagePath(element));
+		
+		if (
+			element.value == ''
+			&& value
+		) {
+			element.value = value;
+		}
+		
+		element.addEventListener('keyup', function(e) {
+			localStorage.setItem(storagePath(e.target), e.target.value);
+		}, false );
+		
+		if (tforms.indexOf(element.form) < 0) {
+			element.form.addEventListener('submit', function(e) {
+				for (var i = 0; i < textareas.length; i++) {
+					var ta = textareas[i];
+					
+					if (ta.form == e.target) {
+						localStorage.removeItem(storagePath(ta));
+					}
+				}
+			}, false );
+			
+			tforms.push(element.form);
+		}
+	}
+	
+	delete tforms;
+
+} )();
