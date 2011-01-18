3bfadf2f451a659c5ad1fd28b197c9b627e7fee5
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-01-18 21:44:26 +0300

    editor merged, but disabled

--- src/js/init.js
+++ src/js/init.js
@@ -1,5 +1,6 @@
 /**
  * @preserve Kalashnikov Igor <igor.kalashnikov@gmail.com>
+ * https://github.com/silentroach/habrafix
  * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  */
 var habrafix = {
