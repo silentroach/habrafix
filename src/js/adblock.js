6377c97ae2036d8a0a9e956114ab5a8547d32e88
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2011-06-02 10:54:45 +0400

    youtube через iframe

--- src/js/adblock.js
+++ src/js/adblock.js
@@ -7,34 +7,43 @@
 	h.config.hideads = new h.configOption('hideads', false, 'Скрывать самую назойливую (сверху) рекламу');
 
 	if (!h.config.hideads.value()) {
-	
+
 		h.dom('a[target=_top]').show();
 		h.dom('object').show();
-		
+		h.dom('iframe').show();
+
 	} else {
-	
+
 		// iframe? O.o
-		h.dom('iframe').remove();
-	
+		h.dom('iframe').each( function() {
+			var frm = this;
+
+			if (!frm.src.match(/youtube.com/)) {
+				h.dom(frm).remove();
+			} else {
+				h.dom(frm).show();
+			}
+		} );
+
 		// показываем ссылки, если они не на pink.habralab
-	
+
 		h.dom('a[target=_top]').each( function() {
 			var link = this;
-	
+
 			if (!link.href.match(/http:\/\/pink.habralab/)) {
 				h.dom(link).show();
 			} else {
 				h.dom(link).remove();
-			}	
+			}
 		} );
-	
+
 		// показываем ранее скрытые в css object, если в них не содержится
 		// ролик с pink.habralab.*
 
 		h.dom('object').each( function() {
-			var 
+			var
 				obj   = this,
-				embed = 
+				embed =
 					h.dom('embed[src]', obj).first() ||
 					h.dom('param[name=movie][value]', obj).first(),
 				param = (!embed) ? false : (h.dom(embed).attr('src') || h.dom(embed).attr('value'));
@@ -48,19 +57,19 @@
 				h.dom(obj).show();
 			}
 		} );
-	
+
 		// удаляем все элементы в #main-page до div.header
 
 		h.dom('#main-page > *').each( function() {
 			var element = this;
-		
+
 			if (
 				element.nodeName == 'DIV'
 				&& element.className == 'header'
 			) {
 				return false;
 			}
-		
+
 			h.dom(element).remove();
 		} );
 
